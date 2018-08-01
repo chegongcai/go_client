73fb814f6c052e1ed363516dd858bb4160838f42
chegongcai <chj040432@sina.com>
2018-08-01 14:19:41 +0800

    client

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go_client/DeviceAndServer"
 	"net"
 	"os"
+	"time"
 )
 
 //182.254.185.142  8080
@@ -28,6 +29,7 @@ func main() {
 		if err != nil {
 			continue
 		}
+		conn.SetDeadline(time.Now().Add(time.Duration(60) * time.Second))
 		go DeviceAndServer.DeviceAndServerConn(conn)
 	}
 }
